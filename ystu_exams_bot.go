3c9754f97b2d5ad0ada3b57bdd33d8df1c31eafa
Михаил Семёнов <124626891+micsemal25@users.noreply.github.com>
2026-06-07 17:29:59 +0300

    Update ystu_exams_bot.go final 2 semestr

--- ystu_exams_bot.go
+++ ystu_exams_bot.go
@@ -173,7 +173,7 @@ func main() {
 				edit := tgbotapi.NewEditMessageTextAndMarkup(
 					update.CallbackQuery.Message.Chat.ID,
 					update.CallbackQuery.Message.MessageID,
-					"*Ответы на экзамен по математике (преп. Ройтенберг В.Ш.) + курс с практикой и ДЗ*\nЦена: 1300 рублей",
+					"*Ответы на экзамен по математике (преп. Ройтенберг В.Ш.) + курс с практикой и ДЗ*\nЦена: 1500 рублей\nПроверка ДЗ: + 500 рублей",
 					payMathCurs,
 				)
 				edit.ParseMode = "Markdown"
@@ -197,7 +197,7 @@ func main() {
 				edit3 := tgbotapi.NewEditMessageTextAndMarkup(
 					update.CallbackQuery.Message.Chat.ID,
 					update.CallbackQuery.Message.MessageID,
-					"*Курс по матану с практикой и ДЗ*\nЦена: 800 рублей",
+					"*Курс по матану с практикой и ДЗ*\nЦена: 1000 рублей\nПроверка ДЗ: + 500 рублей",
 					payCurs,
 				)
 				edit3.ParseMode = "Markdown"
@@ -207,7 +207,7 @@ func main() {
 				}
 
 			case "checkOKMath":
-				links := []string{"https://t.me/+Hm7JCKMS5p80YzQy","https://t.me/+i-1kWLSHbwxiMDU6","https://t.me/+Td6ZKPl55aRlMDk6","https://t.me/+qebWRLDdgrRhYjQy","https://t.me/+Ns6QuCVNsYgyMzRi","https://t.me/+fTlftaWs5IUyMjZi","https://t.me/+zkaabqZRQWAwMzdi","https://t.me/+FkrIykhzoA44Mzcy","https://t.me/+Yz-wlXzePU9jNWVi","https://t.me/+uRIynb6FGWVmNzhi","https://t.me/+ssEyrDQ4ZE4zNTgy","https://t.me/+jvVku9n0aKsxN2Ey","https://t.me/+RNSB52nh4s00YjQ6","https://t.me/+yjEejkckmiY5MmRi","https://t.me/+exfDaoih2yk3NmQy","https://t.me/+k-E6QMv_hnI1YjUy","https://t.me/+e66fLWh5l7I2MjUy","https://t.me/+Lq38ZJWWD7Y3Njk6","https://t.me/+86lS81a67Q00Nzky","https://t.me/+LMLNT4-Ka385ZmQy","https://t.me/+LwOlS5plmRdiNzgy","https://t.me/+GyOnbIQqU_Y3NmVi","https://t.me/+RadF3gvTR-M0ZGEy","https://t.me/+V0gXSsGj1eEwZGJi","https://t.me/+d1ma0Y0ykns3ZmUy","https://t.me/+WsQyULJTG2IwNzhi","https://t.me/+j2D2qjr53_c5NTFi","https://t.me/+MTBA9_IPBVk0OGUy","https://t.me/+jGJa2LXjredhNTBi","https://t.me/+oCucPaT8NDg2NzBi","https://t.me/+aSBmf3iSm-kyYmRi","https://t.me/+LtU7I9IfLY5kNTli","https://t.me/+17grbK7W6zZmYzYy","https://t.me/+0xWmHB5dP383Y2Yy","https://t.me/+chs8DyD8WrhjM2Ni","https://t.me/+Q9i_D8QOGBI5YjEy","https://t.me/+YyNsuuo_vTM5ZmNi","https://t.me/+RY5WPjJ_8P4xOWNi","https://t.me/+bpGO7BvA7SliNjBi","https://t.me/+47C6RsBZWD9kOGEy","https://t.me/+QNBoJXbdoJw5OGUy","https://t.me/+NwrKLqmbMWkxZDAy","https://t.me/+rDgB6OO55Vc0MWMy","https://t.me/+hZagTFEv4b4wYTky","https://t.me/+mfeZypIvd9U5MGUy"}
+				links := []string{"https://t.me/+jGJa2LXjredhNTBi", "https://t.me/+oCucPaT8NDg2NzBi", "https://t.me/+aSBmf3iSm-kyYmRi", "https://t.me/+LtU7I9IfLY5kNTli", "https://t.me/+17grbK7W6zZmYzYy", "https://t.me/+0xWmHB5dP383Y2Yy", "https://t.me/+chs8DyD8WrhjM2Ni", "https://t.me/+Q9i_D8QOGBI5YjEy", "https://t.me/+YyNsuuo_vTM5ZmNi", "https://t.me/+RY5WPjJ_8P4xOWNi", "https://t.me/+bpGO7BvA7SliNjBi", "https://t.me/+47C6RsBZWD9kOGEy", "https://t.me/+QNBoJXbdoJw5OGUy", "https://t.me/+NwrKLqmbMWkxZDAy", "https://t.me/+rDgB6OO55Vc0MWMy", "https://t.me/+hZagTFEv4b4wYTky", "https://t.me/+mfeZypIvd9U5MGUy", "https://t.me/+4qqGtywEQ5s5YjQy", "https://t.me/+qeJivwdWtOxkODMy", "https://t.me/+rNRv_UgdO_E3NTcy"}
 				msg := tgbotapi.NewMessage(chatId[p], "Оплата прошла успешно!✅ Держи ссылку-приглашение в ТГК "+links[m])
 				if _, err := bot.Send(msg); err != nil {
 					log.Panic(err)
@@ -216,7 +216,7 @@ func main() {
 			
 
 			case "checkOKCurs":
-				links := []string{"https://t.me/+khhLP4SViY4zYWVi","https://t.me/+lHnldtl2-IRjMDFi","https://t.me/+fUGO6xTFdDgwYTUy","https://t.me/+d57RhkHGYHJhZWZi","https://t.me/+1QNUY13PMK9hZDZi","https://t.me/+PvSbNvacWmpkMjYy","https://t.me/+QYz6qoYJ_iQyYjZi","https://t.me/+utItj_BLubAxOTY6","https://t.me/+sKk1Dj-YX5U2ZTNi","https://t.me/+q8YswIRdA6E1YWU6","https://t.me/+tXFO4WMM3IlhZTAy","https://t.me/+uIJ7IeO_cgljYzcy","https://t.me/+cliu8tX01i00MjAy","https://t.me/+yVPeV6uw49k3Y2Uy","https://t.me/+1fQ2u2K1839hNTU6","https://t.me/+qUdtE3pDZXA2MDky","https://t.me/+4rCSvSLNUGkxYmUy","https://t.me/+nBoMyYWghbthNTVi","https://t.me/+Vz7lsVnlT5xlYjVi","https://t.me/+r3ZUmivUnJw0YWU6","https://t.me/+rEQnA134Zlk2ZmUy","https://t.me/+lOnLgooQarllZDQy","https://t.me/+axnhKSP-WghkM2Ni","https://t.me/+RedElvwOCjs2YjAy","https://t.me/+UdkkzGAMqRU4YzM6","https://t.me/+9SznMfzebXBmN2Qy","https://t.me/+flqrenV3vBsxY2Iy","https://t.me/+-8VZid8sPR02NTZi","https://t.me/+fXeC4JU6Cm9lYjAy","https://t.me/+reb5zGMdniU4OTEy","https://t.me/+jPrPGCj57rw3Y2Ji","https://t.me/+KlQF4noTUAI4M2Ri","https://t.me/+b3W9Tb5wNxk0MjEy","https://t.me/+U60JCPpkYXY4ZmEy","https://t.me/+V7dk8CKnPxsxYjcy","https://t.me/+JynIZqBj99UzNDNi","https://t.me/+kONOvv1sqaZhYmQy","https://t.me/+OomTRpHWE1w3MmQy","https://t.me/+kDpCn-vsp0UwYTBi","https://t.me/+kMDP6qFgZzRjOTky","https://t.me/+xc5mwza8tHFkNWMy","https://t.me/+71PDJGocNkRmZjk6","https://t.me/+tS8tcysxAigzMzli","https://t.me/+6vkNfg64XMw3MzUy","https://t.me/+hKfMPCCgi3sxOGUy"}
+				links := []string{"https://t.me/+yUtP_1nVe_M2NTky", "https://t.me/+u6vYeC9anKg4MWQy", "https://t.me/+DCotD4Z8dM1jMTEy", "https://t.me/+SA94NpTjguoxY2Ri", "https://t.me/+IEAY3Fqmp0U5NmZi", "https://t.me/+QnBTcG0co3A0NmUy", "https://t.me/+u49tvM-x-_1hMjk6", "https://t.me/+1zYOvKlaZ2AzOTIy", "https://t.me/+72mY8PNWwuhkMjU6", "https://t.me/+UI4IDNTC-jllZmIy", "https://t.me/+VBz6JqLcBYxhZmQy", "https://t.me/+LNUv5XSCBngyMWY6", "https://t.me/+PZZgd6A0DF4zMmU6", "https://t.me/+mua_4CcpRK9hYTky", "https://t.me/+W40cuTOPdZs3MmFi", "https://t.me/+xeEEtiQ7euRhMDY6", "https://t.me/+2pWAeYuHhU1kYjdi", "https://t.me/+ShOu5KSlJwA1MDli", "https://t.me/+gFzXGO4p-p4xNTAy", "https://t.me/+oy8pToMIt-I3YWVi"}
 				msg := tgbotapi.NewMessage(chatId[p], "Оплата прошла успешно!✅ Держи ссылку-приглашение в ТГК "+links[a])
 				if _, err := bot.Send(msg); err != nil {
 					log.Panic(err)
@@ -224,8 +224,8 @@ func main() {
 				a++
 
 			case "checkOKMathCurs":
-				links1 := []string{"https://t.me/+Hm7JCKMS5p80YzQy","https://t.me/+i-1kWLSHbwxiMDU6","https://t.me/+Td6ZKPl55aRlMDk6","https://t.me/+qebWRLDdgrRhYjQy","https://t.me/+Ns6QuCVNsYgyMzRi","https://t.me/+fTlftaWs5IUyMjZi","https://t.me/+zkaabqZRQWAwMzdi","https://t.me/+FkrIykhzoA44Mzcy","https://t.me/+Yz-wlXzePU9jNWVi","https://t.me/+uRIynb6FGWVmNzhi","https://t.me/+ssEyrDQ4ZE4zNTgy","https://t.me/+jvVku9n0aKsxN2Ey","https://t.me/+RNSB52nh4s00YjQ6","https://t.me/+yjEejkckmiY5MmRi","https://t.me/+exfDaoih2yk3NmQy","https://t.me/+k-E6QMv_hnI1YjUy","https://t.me/+e66fLWh5l7I2MjUy","https://t.me/+Lq38ZJWWD7Y3Njk6","https://t.me/+86lS81a67Q00Nzky","https://t.me/+LMLNT4-Ka385ZmQy","https://t.me/+LwOlS5plmRdiNzgy","https://t.me/+GyOnbIQqU_Y3NmVi","https://t.me/+RadF3gvTR-M0ZGEy","https://t.me/+V0gXSsGj1eEwZGJi","https://t.me/+d1ma0Y0ykns3ZmUy","https://t.me/+WsQyULJTG2IwNzhi","https://t.me/+j2D2qjr53_c5NTFi","https://t.me/+MTBA9_IPBVk0OGUy","https://t.me/+jGJa2LXjredhNTBi","https://t.me/+oCucPaT8NDg2NzBi","https://t.me/+aSBmf3iSm-kyYmRi","https://t.me/+LtU7I9IfLY5kNTli","https://t.me/+17grbK7W6zZmYzYy","https://t.me/+0xWmHB5dP383Y2Yy","https://t.me/+chs8DyD8WrhjM2Ni","https://t.me/+Q9i_D8QOGBI5YjEy","https://t.me/+YyNsuuo_vTM5ZmNi","https://t.me/+RY5WPjJ_8P4xOWNi","https://t.me/+bpGO7BvA7SliNjBi","https://t.me/+47C6RsBZWD9kOGEy","https://t.me/+QNBoJXbdoJw5OGUy","https://t.me/+NwrKLqmbMWkxZDAy","https://t.me/+rDgB6OO55Vc0MWMy","https://t.me/+hZagTFEv4b4wYTky","https://t.me/+mfeZypIvd9U5MGUy"}
-				links2 := []string{"https://t.me/+khhLP4SViY4zYWVi","https://t.me/+lHnldtl2-IRjMDFi","https://t.me/+fUGO6xTFdDgwYTUy","https://t.me/+d57RhkHGYHJhZWZi","https://t.me/+1QNUY13PMK9hZDZi","https://t.me/+PvSbNvacWmpkMjYy","https://t.me/+QYz6qoYJ_iQyYjZi","https://t.me/+utItj_BLubAxOTY6","https://t.me/+sKk1Dj-YX5U2ZTNi","https://t.me/+q8YswIRdA6E1YWU6","https://t.me/+tXFO4WMM3IlhZTAy","https://t.me/+uIJ7IeO_cgljYzcy","https://t.me/+cliu8tX01i00MjAy","https://t.me/+yVPeV6uw49k3Y2Uy","https://t.me/+1fQ2u2K1839hNTU6","https://t.me/+qUdtE3pDZXA2MDky","https://t.me/+4rCSvSLNUGkxYmUy","https://t.me/+nBoMyYWghbthNTVi","https://t.me/+Vz7lsVnlT5xlYjVi","https://t.me/+r3ZUmivUnJw0YWU6","https://t.me/+rEQnA134Zlk2ZmUy","https://t.me/+lOnLgooQarllZDQy","https://t.me/+axnhKSP-WghkM2Ni","https://t.me/+RedElvwOCjs2YjAy","https://t.me/+UdkkzGAMqRU4YzM6","https://t.me/+9SznMfzebXBmN2Qy","https://t.me/+flqrenV3vBsxY2Iy","https://t.me/+-8VZid8sPR02NTZi","https://t.me/+fXeC4JU6Cm9lYjAy","https://t.me/+reb5zGMdniU4OTEy","https://t.me/+jPrPGCj57rw3Y2Ji","https://t.me/+KlQF4noTUAI4M2Ri","https://t.me/+b3W9Tb5wNxk0MjEy","https://t.me/+U60JCPpkYXY4ZmEy","https://t.me/+V7dk8CKnPxsxYjcy","https://t.me/+JynIZqBj99UzNDNi","https://t.me/+kONOvv1sqaZhYmQy","https://t.me/+OomTRpHWE1w3MmQy","https://t.me/+kDpCn-vsp0UwYTBi","https://t.me/+kMDP6qFgZzRjOTky","https://t.me/+xc5mwza8tHFkNWMy","https://t.me/+71PDJGocNkRmZjk6","https://t.me/+tS8tcysxAigzMzli","https://t.me/+6vkNfg64XMw3MzUy","https://t.me/+hKfMPCCgi3sxOGUy"}
+				links1 := []string{"https://t.me/+jGJa2LXjredhNTBi", "https://t.me/+oCucPaT8NDg2NzBi", "https://t.me/+aSBmf3iSm-kyYmRi", "https://t.me/+LtU7I9IfLY5kNTli", "https://t.me/+17grbK7W6zZmYzYy", "https://t.me/+0xWmHB5dP383Y2Yy", "https://t.me/+chs8DyD8WrhjM2Ni", "https://t.me/+Q9i_D8QOGBI5YjEy", "https://t.me/+YyNsuuo_vTM5ZmNi", "https://t.me/+RY5WPjJ_8P4xOWNi", "https://t.me/+bpGO7BvA7SliNjBi", "https://t.me/+47C6RsBZWD9kOGEy", "https://t.me/+QNBoJXbdoJw5OGUy", "https://t.me/+NwrKLqmbMWkxZDAy", "https://t.me/+rDgB6OO55Vc0MWMy", "https://t.me/+hZagTFEv4b4wYTky", "https://t.me/+mfeZypIvd9U5MGUy", "https://t.me/+4qqGtywEQ5s5YjQy", "https://t.me/+qeJivwdWtOxkODMy", "https://t.me/+rNRv_UgdO_E3NTcy"}
+				links2 := []string{"https://t.me/+yUtP_1nVe_M2NTky", "https://t.me/+u6vYeC9anKg4MWQy", "https://t.me/+DCotD4Z8dM1jMTEy", "https://t.me/+SA94NpTjguoxY2Ri", "https://t.me/+IEAY3Fqmp0U5NmZi", "https://t.me/+QnBTcG0co3A0NmUy", "https://t.me/+u49tvM-x-_1hMjk6", "https://t.me/+1zYOvKlaZ2AzOTIy", "https://t.me/+72mY8PNWwuhkMjU6", "https://t.me/+UI4IDNTC-jllZmIy", "https://t.me/+VBz6JqLcBYxhZmQy", "https://t.me/+LNUv5XSCBngyMWY6", "https://t.me/+PZZgd6A0DF4zMmU6", "https://t.me/+mua_4CcpRK9hYTky", "https://t.me/+W40cuTOPdZs3MmFi", "https://t.me/+xeEEtiQ7euRhMDY6", "https://t.me/+2pWAeYuHhU1kYjdi", "https://t.me/+ShOu5KSlJwA1MDli", "https://t.me/+gFzXGO4p-p4xNTAy", "https://t.me/+oy8pToMIt-I3YWVi"}
 				msg := tgbotapi.NewMessage(chatId[p], "Оплата прошла успешно!✅ Держи ссылку-приглашение в ТГК по математике "+links1[m]+" и ТГК с курсом по матану"+links2[a])
 				if _, err := bot.Send(msg); err != nil {
 					log.Panic(err)
@@ -256,7 +256,7 @@ func main() {
 				edit := tgbotapi.NewEditMessageText(
 					update.CallbackQuery.Message.Chat.ID,
 					update.CallbackQuery.Message.MessageID,
-					"🤑Оплата 800 руб. на +79997971960 СБП (ВТБ).\nОБЯЗАТЕЛЬНО отправь скрин об оплате!")
+					"🤑Оплата 1000 руб. на +79997971960 СБП (ВТБ).\nОБЯЗАТЕЛЬНО отправь скрин об оплате!")
 
 				if _, err := bot.Send(edit); err != nil {
 					panic(err)
@@ -267,7 +267,7 @@ func main() {
 				edit := tgbotapi.NewEditMessageText(
 					update.CallbackQuery.Message.Chat.ID,
 					update.CallbackQuery.Message.MessageID,
-					"🤑Оплата 1300 руб. на +79997971960 СБП (ВТБ).\nОБЯЗАТЕЛЬНО отправь скрин об оплате!")
+					"🤑Оплата 1500 руб. на +79997971960 СБП (ВТБ).\nОБЯЗАТЕЛЬНО отправь скрин об оплате!")
 
 				if _, err := bot.Send(edit); err != nil {
 					panic(err)
